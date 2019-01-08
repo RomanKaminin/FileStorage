e6deef164c486ea964b7288fec005ba71d8af4b9
kamhome <feliks-roman@mail.ru>
2019-01-08 19:53:51 +0300

    settings

--- django_project/file_storage/settings.py
+++ django_project/file_storage/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'file_storage',
         'PASSWORD': 'file_storage',
-        'HOST': '::',
-        'PORT': '6432',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
@@ -150,10 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
