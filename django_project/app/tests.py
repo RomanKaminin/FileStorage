411d894a8544acd25eb234a94bb17584ed60781c
kamhome <feliks-roman@mail.ru>
2019-02-12 12:58:31 +0300

    add black and flake8

--- django_project/app/tests.py
+++ django_project/app/tests.py
@@ -3,13 +3,10 @@ from app.models import File
 from django.contrib.auth.models import User
 
 
-
 class FileModelTest(unittest.TestCase):
     def create_file(self):
         self.user = User.objects.create_user(
-            username='user1',
-            email='user1@example.com',
-            password='r4r4r4r4'
+            username="user1", email="user1@example.com", password="r4r4r4r4"
         )
         self.file = File.objects.create(
             title="macro.txt",
@@ -18,12 +15,10 @@ class FileModelTest(unittest.TestCase):
             public_link=None,
             file="uploads/2019/doc1.docx",
             is_deleted=False,
-            md5sum='e7df7cd2ca07f4f1ab415d457a6e1c13',
+            md5sum="e7df7cd2ca07f4f1ab415d457a6e1c13",
         )
         return self.file
 
     def test_file_model_creation(self):
         new_file = self.create_file()
         self.assertTrue(isinstance(new_file, File))
-
-
